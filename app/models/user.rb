f85c08d00b704302a573d9e890adb63bd5bb96be
MaxR522 <ranjamario@gmail.com>
2020-10-13 10:31:09 +0300

    add profile

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,8 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+
+  def self.find_by_uid!(uid)
+    User.find_by!("profile_name = :p OR id = :p", p: uid)
+  end
 end
